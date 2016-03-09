d439fdb4244276141277e8738c0db773f7ecbdbb
Saturngod <saturngod@gmail.com>
2016-03-09 12:26:16 +0800

    fixed for Swift 2

--- RabbitConverterTests/RabbitConverterTests.swift
+++ RabbitConverterTests/RabbitConverterTests.swift
@@ -25,25 +25,25 @@ class RabbitConverterTests: XCTestCase {
         // This is an example of a functional test case.
       if let bundlePath = NSBundle(forClass: self.classForCoder).pathForResource("sample", ofType: "json") {
       
-        var json = NSString(contentsOfFile: bundlePath, encoding: NSUTF8StringEncoding, error: nil)
+        let json = try? NSString(contentsOfFile: bundlePath, encoding: NSUTF8StringEncoding)
         
         XCTAssertNotNil(json, "JSON string not found")
         
         
         if let data:NSData = json?.dataUsingEncoding(NSUTF8StringEncoding) {
           
-          var words:NSDictionary = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: nil) as! NSDictionary
+          let words:NSDictionary = (try! NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers)) as! NSDictionary
           
-          var zawgyi: NSArray = words["zg"] as! NSArray
-          var uni: NSArray = words["uni"] as! NSArray
+          let zawgyi: NSArray = words["zg"] as! NSArray
+          let uni: NSArray = words["uni"] as! NSArray
           
 
           var i:Int
           
           for i = 0; i < zawgyi.count ; i++ {
             
-            var unicode:String = uni[i] as! String
-            var zawgyi:String = zawgyi[i] as! String
+            let unicode:String = uni[i] as! String
+            let zawgyi:String = zawgyi[i] as! String
             
             XCTAssertEqual(Rabbit.uni2zg(unicode),zawgyi, "Uni to zawgyi problem")
             XCTAssertEqual(Rabbit.zg2uni(zawgyi),unicode, "Uni to zawgyi problem")
